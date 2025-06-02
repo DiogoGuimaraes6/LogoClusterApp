54c943a7863ffdcaa84814bcb8ddf5e94b513abc
DiogoGuimaraes6 <diogo.guimaraes.projetos@gmail.com>
2025-06-02 10:28:30 +0100

    Add Procfile and use PORT env for Railway deployment

--- app.py
+++ app.py
@@ -149,4 +149,5 @@ def export_png():
     return send_file(buf, mimetype='image/png', as_attachment=True, download_name='logos_compare.png')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5001) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port) 
